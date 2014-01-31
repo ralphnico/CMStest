1c3e034a2147b8ea1474ea00235223a3b777d2b6
ralphnico <rarevalo0528@gmail.com>
2014-01-30 20:36:08 -0500

    lol

--- CMS/Models/User.cs
+++ CMS/Models/User.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace CMS.Models
 {
     public class User
     {
+
         public int id { get; set; }
+        [Required]
         public string username { get; set; }
         public string password { get; set; }
         public string firstname { get; set; }
